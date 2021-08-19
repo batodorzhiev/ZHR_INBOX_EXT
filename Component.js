8700568eda9482f93328525617aae30f54567ff2
Bato <batodor@gmail.com>
2021-08-19 16:31:16 +0300

    Initial
    Initial

---
+++ Component.js
@@ -0,0 +1,26 @@
+jQuery.sap.declare("cross.fnd.fiori.inbox.CA_FIORI_INBOX_EXT.Component");
+
+// use the load function for getting the optimized preload file if present
+sap.ui.component.load({
+	name: "cross.fnd.fiori.inbox",
+	// Use the below URL to run the extended application when SAP-delivered application is deployed on SAPUI5 ABAP Repository
+	url: "/sap/bc/ui5_ui5/sap/CA_FIORI_INBOX"
+		// we use a URL relative to our own component
+		// extension application is deployed with customer namespace
+});
+
+this.cross.fnd.fiori.inbox.Component.extend("cross.fnd.fiori.inbox.CA_FIORI_INBOX_EXT.Component", {
+	metadata: {
+		manifest: "json"
+	},
+	init: function() {
+		this.getService("ShellUIService").then( // promise is returned
+			function(oService) {
+				oService.setTitle("Передача своих функций"); // also could use .getTitle() first
+			},
+			function(oError) {
+				jQuery.sap.log.error("Cannot get ShellUIService", oError, "my.app.Component");
+			}
+		);
+	}
+});
